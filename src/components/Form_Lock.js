4a8ba624fb3525d536d0d2a1029d38aea1cf1021
Ansheng Li <anshengli85@gmail.com>
2023-04-27 22:28:05 -0500

    f

--- src/components/Form_Lock.js
+++ src/components/Form_Lock.js
@@ -1,14 +1,14 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 
 function Form_Lock() {
   return (
-    <Container className='mt-5'>
-        <Form.Text className="text-muted mb-3">
-           Please enter the information to protect/store.
-        </Form.Text>
-        <Form>
+    <Container className="mt-5">
+      <Form.Text className="text-muted mb-3">
+        Please enter the information to protect/store.
+      </Form.Text>
+      <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Account Header</Form.Label>
           <Form.Control type="input" placeholder="Enter Account Name/Header" />
