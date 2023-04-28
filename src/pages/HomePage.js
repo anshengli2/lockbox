7d5d9fbc0c8e8895ce55af635bec4b9e5948f8cc
Ansheng Li <anshengli85@gmail.com>
2023-04-28 12:33:46 -0500

    new new

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AddInfo from "../components/AddInfo";
 
 const HomePage = (props) => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function fetchSample() {
+      const response = await fetch("/.netlify/functions/test");
+      const data = await response.text();
+      console.log(data);
+      setData(data);
+    }
+
+    fetchSample();
+  }, []);
+  const handleClick = () => {
+    fetch("/.netlify/functions/test")
+      .then((response) => response.text())
+      .then((data) => setData(JSON.parse(data)));
+  };
   return (
     <>
       <div>
         <AddInfo />
+        <button onClick={handleClick}>Click</button>
+        <div>
+          data = {data && <p>{data.joke}</p>} {!data && <p>no data</p>}
+        </div>
       </div>
     </>
   );
